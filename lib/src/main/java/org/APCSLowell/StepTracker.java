354827e262b090343e9cabf010cb0ada495d8bab
Kara0721 <karactruong@gmail.com>
2026-04-23 17:09:01 +0000

    done!

--- lib/src/main/java/org/APCSLowell/StepTracker.java
+++ lib/src/main/java/org/APCSLowell/StepTracker.java
@@ -1,8 +1,28 @@
 package org.APCSLowell;
 
 public class StepTracker {
-    public StepTracker(int activityThreshold) {}
-    void addDailySteps(int stepCount) {}
-    int activeDays() {}
-    double averageSteps() {}
+    private int m, dstep, tstep, day, aday;
+    public StepTracker(int activityThreshold) {
+    m = activityThreshold;
+    dstep = 0;
+    tstep = 0;
+    day = 0;
+    aday = 0;
+    }
+    public void addDailySteps(int stepCount) {
+    dstep = stepCount;
+    day++;
+    tstep=tstep+stepCount;
+    if(stepCount>=m)
+        aday++;
+    }
+    public int activeDays() {
+    return aday;
+    }
+    double averageSteps() {
+    if(tstep==0)
+        return 0.0;
+    else
+        return (double)tstep/(double)day;
+        }
 }
